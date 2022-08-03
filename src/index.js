aef412eb7e4e5537efec142e3f6fe9a4bb241412
Vladimir Solovyov <leilyngale@icloud.com>
2022-08-04 00:46:22 +0300

    Added Header, Footer and basic Note components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import App from "./components/App";
 
 ReactDOM.render(
-  <h1>Keeper App Starter File</h1>,
+  <App />,
   document.getElementById("root")
 );
